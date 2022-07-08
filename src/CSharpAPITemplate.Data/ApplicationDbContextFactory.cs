da5812857f80da3313f5e71f08c50eab42f94d02
Valeriy Maslennikov <maslennikovvaleriy@gmail.com>
2022-07-09 00:23:03 +0300

    Another fix for connection string.

--- src/CSharpAPITemplate.Data/ApplicationDbContextFactory.cs
+++ src/CSharpAPITemplate.Data/ApplicationDbContextFactory.cs
@@ -8,7 +8,7 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
 	public ApplicationDbContext CreateDbContext(string[] args)
     {
 	    var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=api-example;User Id=postgres;Password=6vR67wbw;");
+        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=api-example;User Id=postgres;Password=postgres;");
 
         return new ApplicationDbContext(optionsBuilder.Options);
     }
