07fb4371e1975aa339fd20e00efb275930edbab8
Valeriy Maslennikov <maslennikovvaleriy@gmail.com>
2022-07-17 20:29:26 +0300

    Fix file namings

--- src/CSharpAPITemplate/Controllers/CommentController.cs
+++ src/CSharpAPITemplate/Controllers/CommentController.cs
@@ -1,17 +1,17 @@
 ﻿using CSharpAPITemplate.BusinessLayer.Models;
-using CSharpAPITemplate.BusinessLayer.Services.Posts;
+using CSharpAPITemplate.BusinessLayer.Services.Comments;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSharpAPITemplate.Controllers
 {
     [ApiController]
     [ApiVersion("1")]
-    [Route("posts")]
-    public class PostController : BaseController<PostDto>
+    [Route("comments")]
+    public class CommentController : BaseController<CommentDto>
     {
-        public PostController(
-            IPostService postService,
-            ILogger<PostController> logger) : base(postService, logger)
+        public CommentController(
+            ICommentService commentService,
+            ILogger<CommentController> logger) : base(commentService, logger)
         { }
     }
 }
--- src/CSharpAPITemplate/Controllers/PostController.cs
+++ src/CSharpAPITemplate/Controllers/PostController.cs
@@ -1,17 +1,18 @@
 ﻿using CSharpAPITemplate.BusinessLayer.Models;
-using CSharpAPITemplate.BusinessLayer.Services.Comments;
+using CSharpAPITemplate.BusinessLayer.Services.Posts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSharpAPITemplate.Controllers
 {
     [ApiController]
     [ApiVersion("1")]
-    [Route("comments")]
-    public class CommentController : BaseController<CommentDto>
+    [Route("posts")]
+    public class PostController : BaseController<PostDto>
     {
-        public CommentController(
-            ICommentService commentService,
-            ILogger<CommentController> logger) : base(commentService, logger)
-        { }
+        public PostController(
+            IPostService postService,
+            ILogger<PostController> logger) : base(postService, logger)
+        {
+        }
     }
 }
